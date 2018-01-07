ffe73b3a671eeb331a2f3e2cc9d4d6b3fd73bcbb
Aaron Vanston <aaronvanston@gmail.com>
2018-01-07 15:32:26 +1100

    feat: add presentational template

---
+++ templates/presentational/COMPONENT_NAME.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './COMPONENT_NAME.css';
+
+const COMPONENT_NAME = ({}) => (
+  <div className={styles.wrapper}>
+    COMPONENT_NAME
+  </div>
+);
+
+COMPONENT_NAME.propTypes = {};
+
+COMPONENT_NAME.defaultProps = {};
+
+export default COMPONENT_NAME;
